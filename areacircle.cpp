2b261e0e4d3b8fc3491da57b1955cf20a2be9551
souparnikassj <souparnikassj@gmail.com>
2016-08-17 22:38:08 +0530

    Add files via upload

---
+++ areacircle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+  float r,area;
+printf("enter the radius");
+scanf("%f",&r);
+
+area=3.14*r*r;
+printf("area of circle is:%f",area);
+return 0;
+}
