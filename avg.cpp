397fd498edcb7a9bb88d89fd4d1ac7f4a3d22d95
souparnikassj <souparnikassj@gmail.com>
2016-08-17 23:43:21 +0530

    Add files via upload

---
+++ avg.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  float m1,m2,avg,sum;
+printf("enter frst mark");
+scanf("%f",&m1);
+printf("enter scnd mark");
+scanf("%f",&m2);
+sum=m1+m2;
+avg=sum/2;
+printf("average is:%d",avg);
+return 0;
+}
+
+
+
