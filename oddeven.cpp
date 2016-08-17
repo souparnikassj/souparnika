e9fe1bd705bde8b957b2de41bcbd516a9fb53f30
souparnikassj <souparnikassj@gmail.com>
2016-08-18 00:11:13 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a;
+printf("enter a numbr");
+scanf("%d",&a);
+if(a%2==0)
+{
+  printf("a is even");
+}
+else 
+{
+ printf("a is odd");
+}
+return 0;
+}
